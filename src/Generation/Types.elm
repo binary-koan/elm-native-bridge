18c304bc1b0d3967f26b10dc94a8976fd9f0edfc
Jono Mingard <reason.koan@gmail.com>
2017-07-05 23:15:56 +1200

    Start figuring out how to discover types

---
+++ src/Generation/Types.elm
@@ -0,0 +1,68 @@
+module Generation.Types exposing (findTypes)
+
+import Ast.Statement exposing (..)
+import Generation.Annotations exposing (..)
+
+
+type alias GeneratedType =
+    { annotations : List Annotation
+    , declaration : Maybe Statement
+    }
+
+
+type alias Annotation =
+    { name : String
+    , params : List String
+    }
+
+
+findTypes : List Statement -> Result String (List GeneratedType)
+findTypes statements =
+    let
+        findType stmt types =
+            case stmt of
+                Comment text ->
+                    addAnnotations text types
+
+                TypeDeclaration qualifier defs ->
+                    addTypeDeclaration qualifier defs types
+
+                TypeAliasDeclaration qualifier def ->
+                    addTypeAliasDeclaration qualifier def types
+
+                _ ->
+                    Ok types
+    in
+        List.foldl (\stmt types -> Result.andThen (findType stmt) types) (Ok []) statements
+
+
+addAnnotations : String -> List GeneratedType -> Result String (List GeneratedType)
+addAnnotations text types =
+    let
+        firstType =
+            Maybe.withDefault emptyType (List.head types)
+
+        addAnnotationsTo t =
+            { t | annotations = t.annotations ++ findAnnotations text }
+    in
+        case firstType.declaration of
+            Nothing ->
+                Ok <| (addAnnotationsTo firstType) :: Maybe.withDefault [] (List.tail types)
+
+            Just declaration ->
+                Ok <| (addAnnotationsTo emptyType) :: types
+
+
+addTypeDeclaration : Type -> List Type -> List GeneratedType -> Result String (List GeneratedType)
+addTypeDeclaration qualifier defs types =
+    Ok types
+
+
+addTypeAliasDeclaration : Type -> Type -> List GeneratedType -> Result String (List GeneratedType)
+addTypeAliasDeclaration qualifier def types =
+    Ok types
+
+
+emptyType : GeneratedType
+emptyType =
+    { annotations = [], declaration = Nothing }
